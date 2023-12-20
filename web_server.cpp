9f0a25554095695240c6837a84e9cff2801540bf
Nanchar <nanchar220@gmail.com>
2023-12-20 08:26:11 +0530

    1st commit

---
+++ web_server.cpp
@@ -0,0 +1,16 @@
+#include "./httplib.h"
+
+int main() {
+    httplib::Server svr;
+
+    svr.Get("/", [](const httplib::Request& req, httplib::Response& res) {
+        res.set_content("Hello, World!", "text/plain");
+    });
+
+    std::cout << "Server started at http://localhost:8080" << std::endl;
+    svr.listen("localhost", 8080);
+
+    return 0;
+}
+
+
